8ed3ca772ffdab546c8a5bbcad8e53399ca1fef5
qwix <info@example.com>
2008-11-27 16:00:55 +0000

    - fixed wrong email adress
    git-svn-id: http://svn.projects.ez.no/mod_ezclustered_image@13 fda0733b-7bda-4501-8b99-04ae8d7ec120

--- mod_ezclustered_image.c
+++ mod_ezclustered_image.c
@@ -1,5 +1,5 @@
 /**
- * Copyright [2008] [Jérôme Renard jerome.renard@gmail.com] 
+ * Copyright [2008] [Jérôme Renard jr@ez.no] 
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
